6034cdc0bcea4da5b0f33eefbde541897d888e36
RachelDuBpis <rjd5748@rit.edu>
2024-11-05 15:46:53 -0500

    demo 4 done

---
+++ Demo3/nios_system/nios_system_bb.v
@@ -0,0 +1,10 @@
+
+module nios_system (
+	clk_clk,
+	reset_reset_n,
+	leds_export);	
+
+	input		clk_clk;
+	input		reset_reset_n;
+	output	[7:0]	leds_export;
+endmodule
---
+++ Demo3/software/int_demo/obj/default/int_demo.d
@@ -0,0 +1,39 @@
+obj/default/int_demo.o: int_demo.c ../int_demo_bsp//HAL/inc/io.h \
+ ../int_demo_bsp//HAL/inc/alt_types.h system.h ../int_demo_bsp/linker.h \
+ ../int_demo_bsp//HAL/inc/alt_types.h \
+ ../int_demo_bsp//HAL/inc/sys/alt_irq.h ../int_demo_bsp//HAL/inc/nios2.h \
+ ../int_demo_bsp/system.h \
+ ../int_demo_bsp//drivers/inc/altera_avalon_timer_regs.h \
+ ../int_demo_bsp//drivers/inc/altera_avalon_timer.h \
+ ../int_demo_bsp//HAL/inc/sys/alt_dev.h \
+ ../int_demo_bsp//HAL/inc/sys/alt_llist.h \
+ ../int_demo_bsp//HAL/inc/priv/alt_dev_llist.h \
+ ../int_demo_bsp//HAL/inc/sys/alt_warning.h
+
+../int_demo_bsp//HAL/inc/io.h:
+
+../int_demo_bsp//HAL/inc/alt_types.h:
+
+system.h:
+
+../int_demo_bsp/linker.h:
+
+../int_demo_bsp//HAL/inc/alt_types.h:
+
+../int_demo_bsp//HAL/inc/sys/alt_irq.h:
+
+../int_demo_bsp//HAL/inc/nios2.h:
+
+../int_demo_bsp/system.h:
+
+../int_demo_bsp//drivers/inc/altera_avalon_timer_regs.h:
+
+../int_demo_bsp//drivers/inc/altera_avalon_timer.h:
+
+../int_demo_bsp//HAL/inc/sys/alt_dev.h:
+
+../int_demo_bsp//HAL/inc/sys/alt_llist.h:
+
+../int_demo_bsp//HAL/inc/priv/alt_dev_llist.h:
+
+../int_demo_bsp//HAL/inc/sys/alt_warning.h:
